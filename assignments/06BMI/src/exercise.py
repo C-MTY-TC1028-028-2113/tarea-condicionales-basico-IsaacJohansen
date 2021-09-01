cc4858605a754625ed42c086bb0091aca82861e7
Isaac Johansen <A00832598@itesm.mx>
2021-08-31 20:57:01 -0500

    E

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+    if peso > 0 and altura > 0:
+     IMC = peso / altura**2
+     if IMC < 20:
+      print('PESO BAJO')
+     elif 20 <= IMC and IMC <25:
+      print('NORMAL')
+     elif 25 <= IMC and IMC <30:
+      print('SOBREPESO')
+     elif 30 <= IMC and IMC <40:
+      print('OBESIDAD')
+     elif IMC > 40:
+      print('OBESIDAD MORBIDA')
+    else:
+        print('Revisa tus datos, alguno de ellos es erróneo.')
     
 
 if __name__=='__main__':
